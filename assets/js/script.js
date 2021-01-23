146799f5f73873658c6f6dc3576fb5abca46dcd9
Steven Woo <swoo9875@gmail.com>
2021-01-23 18:01:12 -0500

    change the template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
-    //display time function and keep it running every second
+    //display time function using the moment api
     function writeTime() {
-      $(".current-time").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+      $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
       setInterval(writeTime, 1000);
     }
     writeTime();
+
+    $(".btn").click(function () {
+        //local storage
+        //testing to see if the button is working
+        console.log(this);
+        //holds the information for the time
+        var key = $(this).parent().attr("id");
+        //holds the information for textarea
+        var value = $(this).siblings("textarea").val();
+        console.log(key);
+        console.log(value); 
+        //stores the key and value information in the local storage
+        localStorage.setItem(key, value);
+
+    })
 });
+
