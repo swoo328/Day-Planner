d50f31a27ecab4362de49f9dd92d42bb3054b966
Steven Woo <swoo9875@gmail.com>
2021-01-23 20:36:14 -0500

    implementing a function to add colors depending on the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,11 +22,50 @@ $(document).ready(function () {
     })
 });
 //displays the values from the local storage
-var hour = ["hour9", "hour10", "hour11", "hour12", "hour13", "hour14", "hour15", "hour16", "hour17"]
+var hour = ["hour9", "hour10", "hour11", "hour12", "hour13", "hour14", "hour15", "hour16", "hour17"];
 for(var i = 0; i <= hour.length; i++){
     console.log(hour[i]);
     $(`${hour[i]} .description`).val(localStorage.getItem(hour[i]));
 }
 
 
-//
+//time variables
+var m = moment();
+var currentHour = moment().hours();
+console.log(currentHour);
+    //get current hours. should be in military time (24 hour)
+    //console.log(curHour)
+ 
+    function colorHour() {
+        //get current hours. should be in military time (24 hour)
+     
+        //console.log(curHour);
+
+        $(".time").each(function () {
+            var timeHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( timeHour, currentHour)
+
+            //if the current hour is the same as schedule hour the event will be grey
+            if (timeHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            //if the current hour is the same as schedule hour the event will be red
+            else if (timeHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            //if the current hour is the same as schedule hour the event will be greem
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+
+    }
+
+    colorHour()
+
